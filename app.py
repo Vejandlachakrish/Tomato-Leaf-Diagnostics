d369828b194b0dd8b36694c0a1e6beb5a5553614
Vejandlachakrish <112060547+Vejandlachakrish@users.noreply.github.com>
2024-07-09 17:30:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,6 +84,3 @@ def predict():
                                ('static', filename=f'uploads/{filename}'))    
     return redirect(url_for('home'))
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
